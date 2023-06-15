75366ac7da2f5d769dab2c2cf9f2554322386d6c
NikolaasWillaert <nikolaas.willaert@gmail.com>
2023-06-15 08:47:42 +0200

    Added functionality to transfor transcripts of youtube

--- LAIZY.py
+++ LAIZY.py
@@ -1,20 +1,40 @@
 from youtube_transcript_api import YouTubeTranscriptApi
 from youtube_transcript_api.formatters import TextFormatter
+import os
+import streamlit as st
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import ElasticVectorSearch, Pinecone, Weaviate, FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain.llms import OpenAI
 
 import os
 
-video_id = input("Youtube link to transscribe: ")
+video_id = "https://www.youtube.com/watch?v=dD_xNmePdd0"
 video_id = video_id.split('=')[1]
 
 full_text = YouTubeTranscriptApi.get_transcript(video_id, languages=['en'])
 formatter = TextFormatter()
 full_text_formatted = formatter.format_transcript(full_text)
 
-file_name = f"{video_id}.txt"
+os.environ['OPENAI_API_KEY'] = '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'
 
-with open(file_name, 'w') as file:
-    for i in full_text:
-        file.write(i['text'] + " ")
+#split the pdf raw text in chunks spo it can be used by the model
+text_splitter = CharacterTextSplitter(
+    separator='\n',
+    chunk_size=1000,
+    chunk_overlap=200,
+    length_function=len,
+)
+texts = text_splitter.split_text(full_text_formatted)
 
-print(f"File '{file_name}.txt' created and saved.")
+embeddings = OpenAIEmbeddings()
+docsearch = FAISS.from_texts(texts, embeddings)
 
+chain = load_qa_chain(OpenAI(),chain_type="stuff")
+query = st.text_input("Fill out what you want to know about this article")
+
+if query:
+    docs = docsearch.similarity_search(query)
+    answer = chain.run(input_documents=docs, question=query)
+    st.write(answer)
---
+++ README.md
@@ -0,0 +1,5 @@
+## Installation
+
+''' 
+streamlit run LAIZY.py
+'''
