b0ed4e2cae668bdde72430f125e792e08ebbfbc9
NikolaasWillaert <nikolaas.willaert@gmail.com>
2023-06-15 15:37:25 +0200

    updated

--- LAIZY.py
+++ LAIZY.py
@@ -1,39 +1,124 @@
-from youtube_transcript_api import YouTubeTranscriptApi
-from youtube_transcript_api.formatters import TextFormatter
-import os
 import streamlit as st
-from langchain.embeddings.openai import OpenAIEmbeddings
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.vectorstores import ElasticVectorSearch, Pinecone, Weaviate, FAISS
-from langchain.chains.question_answering import load_qa_chain
-from langchain.llms import OpenAI
-import os
+from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
+import langchain.embeddings
+from langchain.vectorstores import FAISS
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from htmlTemplates import css, bot_template, user_template
+from langchain.llms import HuggingFaceHub
 
-os.environ['OPENAI_API_KEY'] = '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'
+from youtube_transcript_api import YouTubeTranscriptApi
 
-chain = load_qa_chain(OpenAI(),chain_type="stuff")
 
-video_id = st.text_input("Youtube Video / Podcast to talk to: ")
-if video_id:
-    video_id = video_id.split('=')[1]
-    full_text = YouTubeTranscriptApi.get_transcript(video_id, languages=['en'])
-    formatter = TextFormatter()
-    full_text_formatted = formatter.format_transcript(full_text)
+def get_text_yt():
+    video_id = "https://www.youtube.com/watch?v=8qgIFD0PaWo"
+    video_id = video_id.split("=")
+    video_id = video_id[1]
+    raw_text = YouTubeTranscriptApi.get_transcript(video_id)
+    string = ""
+    for i in raw_text:
+        string += i['text']
+        string += "\n"
+    return string
 
-    #split the pdf raw text in chunks spo it can be used by the model
+
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+
+def get_text_chunks(text):
     text_splitter = CharacterTextSplitter(
-        separator='\n',
+        separator="\n",
         chunk_size=1000,
         chunk_overlap=200,
-        length_function=len,
+        length_function=len
     )
-    texts = text_splitter.split_text(full_text_formatted)
+    chunks = text_splitter.split_text(text)
+    return chunks
 
-    embeddings = OpenAIEmbeddings()
-    docsearch = FAISS.from_texts(texts, embeddings)
 
-query = st.text_input("Fill out what you want to know about this article")
-if query:
-    docs = docsearch.similarity_search(query)
-    answer = chain.run(input_documents=docs, question=query)
-    st.write(answer)
+def get_vectorstore(text_chunks):
+    #embeddings = OpenAIEmbeddings()
+    embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    return vectorstore
+
+
+def get_conversation_chain(vectorstore):
+    #llm = ChatOpenAI()
+    llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
+
+    memory = ConversationBufferMemory(
+        memory_key='chat_history', return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+        memory=memory
+    )
+    return conversation_chain
+
+
+def handle_userinput(user_question):
+    response = st.session_state.conversation({'question': user_question})
+    st.session_state.chat_history = response['chat_history']
+
+    for i, message in enumerate(st.session_state.chat_history):
+        if i % 2 == 0:
+            st.write(user_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
+
+
+def main():
+    load_dotenv()
+    st.set_page_config(page_title="Chat with multiple PDFs",
+                       page_icon=":books:")
+    st.write(css, unsafe_allow_html=True)
+
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
+
+    st.header("Chat with multiple PDFs :books:")
+    user_question = st.text_input("Ask a question about your documents:")
+    if user_question:
+        handle_userinput(user_question)
+
+    with st.sidebar:
+        st.subheader("Your documents")
+        pdf_docs = st.file_uploader(
+            "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
+        if st.button("Process"):
+            with st.spinner("Processing"):
+                # get pdf text
+                #raw_text = get_pdf_text(pdf_docs)
+                raw_text = get_text_yt()
+                # get the text chunks
+                text_chunks = get_text_chunks(raw_text)
+
+                # create vector store
+                vectorstore = get_vectorstore(text_chunks)
+
+                # create conversation chain
+                st.session_state.conversation = get_conversation_chain(
+                    vectorstore)
+
+
+if __name__ == '__main__':
+    main()
+
+
+
